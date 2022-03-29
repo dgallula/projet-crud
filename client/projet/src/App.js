ea25d652da73c39e4334f674cb8547c6010acc8a
Your Name <you@example.com>
2022-03-29 17:54:56 +0300

    initialiastion server et client

---
+++ client/projet/src/App.js
@@ -0,0 +1,25 @@
+ import './App.css';
+
+function App() {
+  return (
+    <div className="form">
+      <h1>projet Crud</h1>
+
+      <label>table1</label>
+      <input type = 'text' name='table1'/>
+      <label>table2</label>
+      <input type = 'text' name='table2'/>
+      <button>submit</button>
+
+
+
+
+      
+
+
+
+    </div>
+  );
+}
+
+export default App;
